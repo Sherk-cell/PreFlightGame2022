a99a0352f8449d32567871240017fa737e52a81b
RossTubes <70896941+RossTubes@users.noreply.github.com>
2022-09-16 14:03:30 +0200

    Full player movement
    Full player movement + camera movemet + throwing objects

---
+++ GrenadeThrow.cs
@@ -0,0 +1,110 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrenadeThrow : MonoBehaviour
+{
+    public int damage;
+    //public bool destroyOnHit;
+
+    [Header("Explosive Projectile")]
+    public bool isExplosive;
+    public float explosionRadius;
+    public float explosionForce;
+    public int explosionDamage;
+    public GameObject explosionEffect;
+
+    private Rigidbody rb;
+
+    private bool targetHit;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        // make sure only to stick to the first target you hit
+        if (targetHit)
+            return;
+        else
+            targetHit = true;
+
+        // check if you hit an enemy
+        if (collision.gameObject.GetComponent<Enemy>() != null)
+        {
+            Enemy enemy = collision.gameObject.GetComponent<Enemy>();
+
+            enemy.TakeDamage(damage);
+
+            // destroy projectile after kill
+            Destroy(gameObject);
+
+            // destroy projectile
+            /*if (!isExplosive) //destroyOnHit
+                Invoke(nameof(DestroyProjectile), 0.1f);*/
+        }
+
+        // explode projectile if it's explosive
+        if (isExplosive)
+        {
+            Explode();
+            return;
+        }
+    }
+    private void Explode()
+    {
+        // spawn explosion effect (if assigned)
+        if (explosionEffect != null)
+            Instantiate(explosionEffect, transform.position, Quaternion.identity);
+
+        // find all the objects that are inside the explosion range
+        Collider[] objectsInRange = Physics.OverlapSphere(transform.position, explosionRadius);
+
+        // loop through all of the found objects and apply damage and explosion force
+        for (int i = 0; i < objectsInRange.Length; i++)
+        {
+            if (objectsInRange[i].gameObject == gameObject)
+            {
+                // don't break or return please, thanks
+            }
+            else
+            {
+                // check if object is enemy, if so deal explosionDamage
+                if (objectsInRange[i].GetComponent<Enemy>() != null)
+                    objectsInRange[i].GetComponent<Enemy>().TakeDamage(explosionDamage);
+
+                // check if object has a rigidbody
+                if (objectsInRange[i].GetComponent<Rigidbody>() != null)
+                {
+                    // custom explosionForce
+                    Vector3 objectPos = objectsInRange[i].transform.position;
+
+                    // calculate force direction
+                    Vector3 forceDirection = (objectPos - transform.position).normalized;
+
+                    // apply force to object in range
+                    objectsInRange[i].GetComponent<Rigidbody>().AddForceAtPosition(forceDirection * explosionForce + Vector3.up * explosionForce, transform.position + new Vector3(0, -0.5f, 0), ForceMode.Impulse);
+
+                    Debug.Log("Kabooom " + objectsInRange[i].name);
+                }
+            }
+        }
+
+        // destroy projectile with 0.1 seconds delay
+        //Invoke(nameof(DestroyProjectile), 0.1f);
+    }
+
+    /*private void DestroyProjectile()
+    {
+        Destroy(gameObject);
+    }*/
+
+    // just graphics stuff
+    private void OnDrawGizmosSelected()
+    {
+        Gizmos.color = Color.red;
+        Gizmos.DrawWireSphere(transform.position, explosionRadius);
+    }
+}
---
+++ throwing.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class throwing : MonoBehaviour
+{[Header("References")]
+    public Transform cam;
+    public Transform attackPoint;
+    public GameObject objectToThrow;
+
+    public GameObject secondObjectToThrow;
+
+    [Header("Settings")]
+    public int totalThrows;
+    public float throwCooldown;
+
+    public int totalGrenades;
+    public float grenadeCooldown;
+
+    [Header("Throwing")]
+    public KeyCode throwKey = KeyCode.Mouse0;
+    public KeyCode GrenadeKey = KeyCode.Mouse1;
+    public float throwForce;
+    public float throwUpwardForce;
+
+    public float throwGrenadeForce;
+    public float throwUpwardNadeForce;
+
+    bool readyToThrow;
+    bool readyToGrenade;
+
+    private void Start()
+    {
+        readyToThrow = true;
+
+        readyToGrenade = true;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(throwKey) && readyToThrow && totalThrows > 0)
+        {
+            Throw();
+        }
+
+        if (Input.GetKeyDown(GrenadeKey) && readyToGrenade && totalGrenades > 0)
+        {
+            GrenadeThrow();
+        }
+    }
+
+    private void GrenadeThrow()
+    {
+        readyToGrenade = false;
+
+        // instantiate object to throw
+        GameObject projectile = Instantiate(secondObjectToThrow, attackPoint.position, cam.rotation);
+
+        // get rigidbody component
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        Vector3 forceDirection = cam.transform.forward;
+
+        // add force
+        Vector3 forceToAdd = forceDirection * throwGrenadeForce + transform.up * throwUpwardNadeForce;
+
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        totalGrenades--;
+
+        // implement throwCooldown
+        Invoke(nameof(ResetGrenadeThrow), grenadeCooldown);
+    }
+    private void Throw()
+    {
+        readyToThrow = false;
+
+        // instantiate object to throw
+        GameObject projectile = Instantiate(objectToThrow, attackPoint.position, cam.rotation);
+
+        // get rigidbody component
+        Rigidbody projectileRb = projectile.GetComponent<Rigidbody>();
+
+        // calculate direction
+        Vector3 forceDirection = cam.transform.forward;
+
+        //RaycastHit hit;
+
+        /*if(Physics.Raycast(cam.position, cam.forward, out hit, 500f))
+        {
+            forceDirection = (hit.point - attackPoint.position).normalized;
+        }*/
+
+        // add force
+        Vector3 forceToAdd = forceDirection * throwForce + transform.up * throwUpwardForce;
+
+        projectileRb.AddForce(forceToAdd, ForceMode.Impulse);
+
+        totalThrows--;
+
+        // implement throwCooldown
+        Invoke(nameof(ResetThrow), throwCooldown);
+    }
+
+    private void ResetThrow()
+    {
+        readyToThrow = true;
+    }
+
+    private void ResetGrenadeThrow()
+    {
+        readyToGrenade = true;
+    }
+}
